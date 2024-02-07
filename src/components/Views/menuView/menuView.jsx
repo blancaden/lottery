d88109cf2015d33715bd63cfb6bb7bbde418d77b
Blanca <blankdgonzfigue@gmail.com>
2024-02-07 14:55:51 +0100

    UPDATE IMG

--- src/components/Views/menuView/menuView.jsx
+++ src/components/Views/menuView/menuView.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const menuView = () => {
   return (
     <>
-        <div className="img-homeView-cole" ><img src="img/img-homeView-cole.jpg" alt="santa-tecla" /></div>
+        <div className="img-homeView-cole" >
+            <img src="img/img" alt="santa-tecla" />
+        </div>
 
     </>
   )
