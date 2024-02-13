0391f1adf4e14a02e50292aea971add001fa23fd
Roger German Cecconello <nia.luna12@gmail.com>
2024-02-13 13:03:32 +0100

    added roulette

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,23 +5,25 @@ import './Footer.css'
 const Footer = () => {
   return (
     <>
+    <div className='footer-container'>
       <footer className="footer_home">
-    <div className="comment1">
+       <div className="comment1">
         <p> &copy;2024 G3 Tecnologys</p>
-    </div>
+       </div>
 
-        <div id="comment2">
+       <div id="comment2">
 
                 <img  src="img/telefono.png" />
                 <p>Teléfono 900234353</p>  
-        </div>
+       </div>
     
         <div id="comment3">
             <img src="img/maps.png" />
             <p> Dirección Avda Ramon y Cajal 20 </p>
 
         </div>
-    </footer>
+     </footer>
+    </div>
     </>
   )
 }
